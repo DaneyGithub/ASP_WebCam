2e62daa59a6410969252e7f279e3c40d6ac6a6a1
Daniel Mekonnen <daneymail@gmail.com>
2017-11-13 14:25:48 +0100

    Add project files.

---
+++ ASP_WebCam/Controllers/RobotController.cs
@@ -0,0 +1,76 @@
+﻿using ASP_WebCam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+
+namespace ASP_WebCam.Controllers
+{
+    public class RobotController : Controller
+    {
+
+        private FilterInfoCollection webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        private VideoCaptureDevice cam;
+
+       
+        public void LoadMine()
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        }
+        public ActionResult Webcam()
+        {
+            Assign();
+            cam = new VideoCaptureDevice(webcam[0].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+            return View("Index");
+        }
+
+        
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+           // pictureBox1.Image = bit;
+           
+        }
+
+
+        public ActionResult Assign()
+        {
+            RobotModel name = new RobotModel { Start = "Text through Model", VideoFileE= "Content/video.mp4" };
+            return View(name);
+        }
+        
+        // GET: Robot
+        public ActionResult Index()
+        {
+            ViewBag.MyText = "Passing text from Conttoller to View";
+            Assign();
+            return View();
+        }
+
+        public ActionResult Start()
+        {
+
+          /* 
+           * //the selected cam
+           *cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+
+            */
+            Assign();
+            return View("Index");
+        }
+
+        public ActionResult Stop()
+        {
+            Assign();
+            return View("Index");
+        }
+    }
+}
---
+++ ASP_WebCam/Models/RobotModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace ASP_WebCam.Models
+{
+    public class RobotModel
+    {
+
+        public int Counter { get; set; }
+        public string Start { get; set; }
+        public string YoutubeLink { get; set; }
+        public string VideoFileE { get; set; }
+        public string ChangeText = "What";
+        public Image MyBitImage { get; set; }
+        public string MyImagePath { get; set; }
+        public byte[] ImageBytes { get; set; }
+
+    }
+
+    
+}
